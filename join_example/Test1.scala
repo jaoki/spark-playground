4d78de7cc0b8ccdbe7c80087330d7ada0e7dd55a
jaoki <you@example.com>
2015-09-30 21:50:50 -0700

    added join

--- join_example/Test1.scala
+++ join_example/Test1.scala
@@ -1,16 +1,23 @@
 
-var data1 = sc.textFile("join_example/data1.tsv").map(_.split("\t"))
-var data2 = sc.textFile("join_example/data2.tsv").map(_.split("\t"))
+object Test1 extends java.io.Serializable{
+  case class Data1(date: String, uid: String, count: Int)
+  case class Data2(date: String, uid: String, count: Int, a: Float, b:Float)
 
-case class Data1(date: String, uid: String, count: Int)
-case class Data2(date: String, uid: String, count: Int, a: Float, b:Float)
+//  val parse1 = (l:Array[String]) => {(l(1), Data1(l(0), l(1), l(2).toInt))}
+  def main(args: Array[String]){
 
-var data11 = data1.map(l => (l(1), Data1(l(0), l(1), l(2).toInt)))
-// var data11 = data1.map((_(1), Data1(_(0), _(1), _(2).toInt)))
-//var data11 = data1.map(println(_))
+    var data1 = sc.textFile("join_example/data1.tsv").map(_.split("\t"))
+    var data2 = sc.textFile("join_example/data2.tsv").map(_.split("\t"))
 
-var data21 = data2.map(l => (l(1), Data2(l(0), l(1), l(2).toInt, l(3).toFloat, l(4).toFloat)))
-
-var joinedData = data11.join(data21)
-joinedData.foreach(r => println(r._1 + ":" + r._2._1.count + ":" + r._2._2.count))
+    var data11 = data1.map(l => (l(1), Data1(l(0), l(1), l(2).toInt)))
+//    var data11 = data1.map(l => parse1(l))
+//    // var data11 = data1.map((_(1), Data1(_(0), _(1), _(2).toInt)))
+//    //var data11 = data1.map(println(_))
+//
+    var data21 = data2.map(l => (l(1), Data2(l(0), l(1), l(2).toInt, l(3).toFloat, l(4).toFloat)))
+//
+    var joinedData = data11.join(data21)
+    joinedData.foreach(r => println(r._1 + ":" + r._2._1.count + ":" + r._2._2.count))
+  }
+}
 
