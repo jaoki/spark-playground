34288b23e0781867b09c1884d7d8990df26fc04d
jaoki <you@example.com>
2015-10-04 01:49:31 -0700

    moved

--- wordcount1/src/main/scala/wordcount1/test1.scala
+++ wordcount1/src/main/scala/wordcount1/test1.scala
@@ -2,12 +2,12 @@ package wordcount1
 
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.rdd.MappedRDD
 
 object Main{
   def main(args: Array[String]) : Unit = {
     val SPARK_HOME = sys.env("SPARK_HOME")
     val sc = new SparkContext("local", "WordCount1", SPARK_HOME)
+    sc.setLogLevel("WARN")
 
 
     val readmeMdRdd = sc.textFile(SPARK_HOME + "/README.md")
@@ -17,7 +17,10 @@ object Main{
     wordcount.filterTest()
     wordcount.mapTest()
 
-
+    println("word count result ==================")
+    val result:RDD[(String, Int)] = wordcount.wordCount()
+    result.foreach(println)
+    result.count()
 
   }
 }
@@ -46,33 +49,22 @@ class WordCount1(data: RDD[String]) extends Serializable{
 	}
 
 
+	def properlySplit(line: String) : Array[String] = {
+		val delimiters:Array[String] = Array(" ", "\"", ".", ",", "\\[", "]", "\\(", ")", "=", "-", "<", ">", "#", ":", "`")
+		for(d <- delimiters){
+			if(line.contains(d)){
+				return line.split(d).flatMap(properlySplit)
+			}
+		}
+		Array(line)
+	}
 
-//	def properlySplit(line: String) : Array[String] = {
-//		var delimiters:Array[String] = Array(" ", "\"", ".", ",", "\\[", "]", "\\(", ")", "=", "-", "<", ">", "#", ":", "`")
-//		for(d <- delimiters){
-//			if(line.contains(d)){
-//				return line.split(d).flatMap(properlySplit)
-//			}
-//		}
-//		Array(line)
-//	}
-//
-//	def wordCount() : RDD[(String, Int)] = {
-//		// var allWords = readmeMdFile.flatMap(line => line.split(" ")).map(a => a.st)
-//		var allWords = readmeMdFile.flatMap(properlySplit)
-//		var wordPairs = allWords.map(word => (word.toLowerCase(), 1))
-//		var countsUnsorted = wordPairs.reduceByKey((a:Int, b:Int) => a+b)
-//		// countsUnsorted.sortByKey(false)
-//		countsUnsorted.sortBy(_._1)
-//	}
+	def wordCount() : RDD[(String, Int)] = {
+		// var allWords = readmeMdFile.flatMap(line => line.split(" ")).map(a => a.st)
+		var allWords = data.flatMap(properlySplit)
+		var wordPairs = allWords.map(word => (word.toLowerCase(), 1))
+		var countsUnsorted = wordPairs.reduceByKey((a:Int, b:Int) => a+b)
+		// countsUnsorted.sortByKey(false)
+		countsUnsorted.sortBy(_._1)
+	}
 }
-//
-//
-//// filterTest()
-//// mapTest()
-//// max(1, 2)
-//println("word count result ==================")
-//var wordCounts:RDD[(String, Int)] = new Test().wordCount()
-//wordCounts.foreach(println)
-//wordCounts.count()
-
